19e0f91b6ef7860c7a02d3400d1e28d0959053ba
lmd Linux <lmd.linux@gmail.com>
2023-10-22 23:34:25 -0600

    Add changelog generation
    Automatically generate debian package changelog from git history. This
    allows multi-line git commit messages and multiple commits per package
    release.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ log() {
     echo -e "\033[0;32m${@}\033[0m"
 }
 
+changelog_line() {
+    echo "${@}" >> debian/changelog
+}
+
 apt-get -y update
 apt-get -y install \
     ca-certificates \
@@ -32,6 +36,34 @@ git clone --depth 1 git@github.com:lmd-linux/lmd-linux.github.io.git
 
 log "Building ${REPO##*/}"
 cd ${REPO##*/}
+
+rm -f debian/changelog
+COMMITS=$(git rev-list HEAD -- *)
+PREVIOUS_COMMIT=""
+PREVIOUS_VERSION=""
+for COMMIT in ${COMMITS}; do
+    VERSION=$(git show ${COMMIT}:debian/version)
+    if [[ ${PREVIOUS_VERSION} != ${VERSION} ]]; then
+        if [[ -n ${PREVIOUS_COMMIT} ]]; then
+            changelog_line
+            changelog_line " -- lmd Linux <lmd.linux@gmail.com>  ${DATE}"
+            changelog_line
+        fi
+        DATE=$(git log --format=%cD -n 1 ${COMMIT})
+        URGENCY=$(git show ${COMMIT}:debian/urgency)
+        changelog_line "${REPO##*/} (${VERSION}) all; urgency=${URGENCY}"
+        changelog_line
+    fi
+    MESSAGE=$(git log --format=%B -n 1 ${COMMIT})
+    changelog_line "${MESSAGE}" | sed -e '/^$/d' -e 's/^/    /' -e '1s/^    /  * /'
+    changelog_line "    https://github.com/${REPO}/commit/${COMMIT:0:8}"
+    PREVIOUS_COMMIT=${COMMIT}
+    PREVIOUS_VERSION=${VERSION}
+done
+DATE=$(git log --format=%cD -n 1 ${COMMIT})
+changelog_line
+changelog_line " -- lmd Linux <lmd.linux@gmail.com>  ${DATE}"
+
 debuild -uc -us
 
 log "Publishing ${REPO##*/}"
